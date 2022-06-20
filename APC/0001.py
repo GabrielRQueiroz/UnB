28f3ef72e3608ce380d5693f0493eeb8987fc71f
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2022-06-20 15:13:27 -0300

    🧪 test: Pytest setup

--- APC/0001.py
+++
@@ -1,9 +0,0 @@
-print("Hello, world!")
-
-
-def foo(bar):
-    return f"{bar} being tested"
-
-
-print(foo(1))
-
---
+++ APC/tests/test_foobar.py
@@ -0,0 +1,6 @@
+from foobar import foo
+
+
+def test_func():
+    assert foo(1) == "1 is being tested"
+
