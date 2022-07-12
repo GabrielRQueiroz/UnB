00eb31ed78afe9accf6e824f8b0246e9a7f89c8a
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2022-07-12 19:04:35 -0300

    🧪 test(Parcelas): Tests setup
    Added test cases for parcelas.py

--- APC/parcelas.py
+++ APC/parcelas.py
@@ -6,12 +6,8 @@ disc_table = {
     4: 1.1,
 }
 
+
 def f_price(price, times):
     final_price = price * disc_table[times]
-    price_per_time = final_price/times
+    price_per_time = final_price / times
     return f"{final_price:.2f} {price_per_time:.2f}"
-    
-print(f_price(150, 4))
-print(f_price(200, 3))
-print(f_price(45, 1))
-print(f_price(70.25, 2))
