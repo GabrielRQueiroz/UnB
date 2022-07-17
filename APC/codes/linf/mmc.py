fe5c1bbb622368279863dc061b65bd0d337159b6
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2022-07-17 02:10:34 -0300

    🌈 style(APC): Styling
    Minor code indentation and styling changes

--- APC/codes/linf/mmc.py
+++ APC/codes/linf/mmc.py
@@ -1,14 +1,14 @@
-def mmc(numbers):
+def mmc(numbers=[1, 1]):
     numbers.sort()
     num_a, num_b = numbers
-    
+
     if num_a < 0 or num_b < 0:
         return
-    
+
     elif num_a == 0 or num_b == 0:
         return 0
-    
-    elif num_b%num_a == 0:
+
+    elif num_b % num_a == 0:
         return num_b
-    
-    return num_a*num_b / (num_b%num_a)
+
+    return num_a * num_b / (num_b % num_a)
