fb7315c3b8259c07877a96aa392cfe112db28e9e
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-01-20 15:57:48 -0300

    Meu git não atualiza >:(

--- ED/recursion/sabacchard.py
+++ ED/recursion/sabacchard.py
@@ -1,24 +1,25 @@
 cards = int(input())
 cards_list = [int(card) for card in input().split()]
 
+combinations = [[0 for j in range(cards)] for i in range(cards)]
 
-def sabacchard_game(cards_list):
-    if len(cards_list) == 0:
+
+def sabacchard_game(i, j):
+    if i >= j:
         return 0
+    elif i - j == 1:
+        return max(cards_list[i], cards_list[j])
 
-    elif len(cards_list) == 1:
-        return cards_list[0]
+    if combinations[i][j]:
+        return combinations[i][j]
 
-    elif len(cards_list) == 2:
-        return max(cards_list[0], cards_list[-1])
-    
-    else:
-        best = max(*cards_list[:2:], *cards_list[-2::])
+    combinations[i][j] = max(
+        max(cards_list[i], cards_list[j]) + sabacchard_game(i + 1, j - 1),
+        cards_list[j] + sabacchard_game(i, j - 2),
+        cards_list[i] + sabacchard_game(i + 2, j),
+    )
 
-        cards_list.remove(min(*cards_list[:2:], *cards_list[-2::]))
-        cards_list.remove(best)
-
-        return best + sabacchard_game(cards_list)
+    return combinations[i][j]
 
 
-print(sabacchard_game(cards_list))
+print(sabacchard_game(0, cards - 1))
